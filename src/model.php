513b264aeec89e7b9cbdc84211d7960e46f7bd74
DidonCode <perrier.richardj@gmail.com>
2023-10-18 14:00:33 +0200

    Crée un template et un layout

--- src/model.php
+++ src/model.php
@@ -62,7 +62,7 @@ function getComments($identifier)
 function dbConnect()
 {
     try {
-        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
 
         return $database;
     } catch(Exception $e) {
